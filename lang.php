e3c01318387f33ca0b993f1d643a1905e53b7593
Raphal Ricordeau <r77raphy@gmail.com>
2014-08-20 22:35:13 +0200

    Initialisation de l'outil de creation de la base de donnees

---
+++ lang.php
@@ -0,0 +1,10 @@
+<?php
+define("_INSTALL_PAGE_WELCOME_MESSAGE","Bienvenu sur la page de création de la base de données pour l'outils naheul_tool");
+define("_INSTALL_PAGE_TITLE","Page de configuration et installation de naheul_tool");
+define("_INSTALL_PAGE_QUESTION","Quel action souhaitez vous effectuer ?");
+define("_INSTALL_PAGE_CHOICE_CREATE","Créer la base de données depuis zero (besoin mot de passe root de la DB)");
+define("_INSTALL_PAGE_CHOICE_INSTALL","Création des tables utilisé par l'application");
+define("_INSTALL_PAGE_CHOICE_CONFIG","Reconfiguration de l'accès à la base de données");
+define("_CREATE_PAGE_QUESTION","Quel est le mot de passe root pour la connexion à la base de données ?");
+define("_CREATE_PAGE_CONNECT_ERROR","Impossible de ce connecter veuillez vérifier le mot de passe et le ré-entrer à la page précédente.")
+?>
